67cf196c9480467aefa29531728d77bec23176ff
Gabriel <gvs.070104@gmail.com>
2019-07-01 17:12:31 -0300

    sistema de exclusão de elementos adicionada

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+var idi = 0;
 function adicionar() {
     var table = document.querySelector('#tabela');
     
@@ -6,6 +7,7 @@ function adicionar() {
     var inputGenero = document.querySelector('#genero');
    
     var linha = document.createElement('tr');
+    linha.setAttribute('id', idi);
     
     var conN = document.createElement('td');
     conN.appendChild(document.createTextNode(inputNome.value));
@@ -14,12 +16,26 @@ function adicionar() {
     var conG = document.createElement('td');
     conG.appendChild(document.createTextNode(inputGenero.value));
 
+    var excluir = document.createElement('td');
+    var botao = document.createElement('button');
+    botao.appendChild(document.createTextNode('Excluir'));
+    botao.setAttribute('onclick', 'excluir('+idi+')');
+    excluir.appendChild(botao);
+
+
     linha.appendChild(conN);
     linha.appendChild(conP);
     linha.appendChild(conG);
+    linha.appendChild(excluir);
 
     table.appendChild(linha);
     inputNome.value = '';
     inputPagina.value = '';
     inputGenero.value = '';
+    idi++;
+}
+function excluir(i) {
+    var table = document.querySelector('#tabela');
+    var linhaE = document.getElementById(i);
+    table.removeChild(linhaE);
 }
