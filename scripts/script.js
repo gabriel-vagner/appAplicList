518bf64298b700a14e17ba3928295a2c6f374970
NDLemes <ndlemes7@outlook.com>
2019-07-03 14:51:37 -0300

    Postando as alterações feitas em sala por causa do bug / Terminado o Storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,63 @@
-class Livro{
-    constructor(n, p, g){
+class Livro {
+    constructor(n, p, g, i) {
         this.Nome = n;
         this.Pagina = p;
         this.Genero = g;
+        this.Id = i;
     }
-
-    getNome(){
+    getNome() {
         return this.Nome;
     }
-    getPagina(){
+    getPagina() {
         return this.Pagina;
     }
-    getGenero(){
+    getGenero() {
         return this.Genero;
     }
 }
-
-
-
 var idi = JSON.parse(localStorage.getItem("idolos"));
 
-function restore() {
-for (let i = 0; i < idi ; i++) {
-        console.log("exe");
-    if (localStorage.getItem(i)) {
-        console.log("achei");
-    }
-}  
+function restore() { 
+    for (let i = 0; i < idi; i++) {
+        if (localStorage.getItem(i)) {
+            var table = document.querySelector('table');
+            var res = JSON.parse(localStorage.getItem(i));
+            
+            var linha = document.createElement('tr');
+            linha.setAttribute('id', res.Id);
 
+            var conN = document.createElement('td');
+            conN.appendChild(document.createTextNode(res.Nome));
+            var conP = document.createElement('td');
+            conP.appendChild(document.createTextNode(res.Pagina));
+            var conG = document.createElement('td');
+            conG.appendChild(document.createTextNode(res.Genero));
+
+            var excluir = document.createElement('td');
+            var botao = document.createElement('button');
+            botao.appendChild(document.createTextNode('Excluir'));
+            botao.setAttribute('onclick', 'excluir(' + res.Id + ')');
+            excluir.appendChild(botao);
+
+            linha.appendChild(conN);
+            linha.appendChild(conP);
+            linha.appendChild(conG);
+            linha.appendChild(excluir);
+            
+            table.appendChild(linha);
+        }
+    }
 }
-restore();
 function adicionar() {
     var table = document.querySelector('#tabela');
-    
     var inputNome = document.querySelector('#nome');
     var inputPagina = document.querySelector('#pagina');
     var inputGenero = document.querySelector('#genero');
-   
+
     var linha = document.createElement('tr');
     linha.setAttribute('id', idi);
-    
-    var novoLivro = new Livro(inputNome.value, inputPagina.value, inputGenero.value);
+
+    var novoLivro = new Livro(inputNome.value, inputPagina.value, inputGenero.value, idi);
     console.log(novoLivro);
 
     var conN = document.createElement('td');
@@ -53,9 +70,8 @@ function adicionar() {
     var excluir = document.createElement('td');
     var botao = document.createElement('button');
     botao.appendChild(document.createTextNode('Excluir'));
-    botao.setAttribute('onclick', 'excluir('+idi+')');
+    botao.setAttribute('onclick', 'excluir(' + idi + ')');
     excluir.appendChild(botao);
-
 
     linha.appendChild(conN);
     linha.appendChild(conP);
@@ -68,7 +84,6 @@ function adicionar() {
     inputGenero.value = '';
 
     localStorage.setItem(idi, JSON.stringify(novoLivro));
-    //localStorage.setItem('idolos', JSON.stringify(0));
     idi++;
     localStorage.setItem('idolos', JSON.stringify(idi));
 }
@@ -78,3 +93,4 @@ function excluir(i) {
     table.removeChild(linhaE);
     localStorage.removeItem(i);
 }
+restore();
